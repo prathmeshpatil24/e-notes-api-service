655d2390a54018bb68c8f2156543ce5bf1862caf
prathmeshpatil24 <patilprathmesh365@gmail.com>
2025-09-20 17:11:50 +0530

    initial commit

---
+++ src/main/java/com/enotes/AppController.java
@@ -0,0 +1,17 @@
+package com.enotes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AppController {
+
+    @GetMapping("/test")
+    public String testPage() {
+        System.out.println("!!! This is a new print statement !!!");
+
+        return "test-page";
+    }
+}
